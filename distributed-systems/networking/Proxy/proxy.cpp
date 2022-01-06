b181577bbe8f2601d47c698ab51510d7d7dbf09e
ldjogo2 <bball.luka.hotmail.com@gmail.com>
2022-01-06 15:25:58 -0600

    Starting legit networking

---
+++ distributed-systems/networking/Proxy/proxy.cpp
@@ -0,0 +1,5 @@
+// https://www.varonis.com/blog/what-is-a-proxy-server
+// https://www.fortinet.com/resources/cyberglossary/proxy-vs-vpn
+// In order to understand how these servers work in encrypting our data, we must first understand how IP addresses work
+// and how things are communicated over these IP addresses. I don't know if I wrote this correctly, but I need to go
+// over the foundations of networking.
